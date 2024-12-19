ee781b96126948f5ab178447af8f958f4d4becc7
SadhwaniMoomal <sadhwanimoomal987@gmail.com>
2024-12-19 22:56:17 +0500

    update file

---
+++ lecture_1/index.py
@@ -0,0 +1,13 @@
+# varaibles
+# StudentName = 'Moomal Sadhwani'
+# DecResgistion = 5000
+# TutionFees = 4000
+
+# totalAmount = DecResgistion + TutionFees
+# print (StudentName, "has paid" , totalAmount)
+
+a = 2
+b = 3
+result = (a**2 + (2 * a * b) + b**2)
+print(result)
+
